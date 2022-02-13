a822bd19b2f0980a2070aa2aa0e52f4fda252a8d
giovanialvin <99602640+giovanialvin@users.noreply.github.com>
2022-02-13 22:36:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    include "Mahasiswa.php";
+
+    $mhs1 = new Mahasiswa();
+    $mhs1->setNim("2003721");
+    $mhs1->setNama("Alvin Giovani");
+    $mhs1->setKelamin("Pria");
+    $mhs1->setProdi("Ilmu Komputer");
+    $mhs1->setSemester("4");
+
+    $mhs2 =  new Mahasiswa("2000000", "Andin", "Wanita", "Manajemen Kewanitaan", "6");
+    
+    echo "NIM : ". $mhs1->getNim()."<br>";
+    echo "Nama : ". $mhs1->getNama() ."<br>";
+    echo "Kelamin : ". $mhs1->getKelamin()."<br>";
+    echo "Prodi : ". $mhs1->getProdi() ."<br>";
+    echo "Semester : ". $mhs1->getSemester() ."<br>";
+
+    echo "<br>";
+
+    echo "NIM : ". $mhs2->getNim()."<br>";
+    echo "Nama : ". $mhs2->getNama() ."<br>";
+    echo "Kelamin : ". $mhs2->getKelamin()."<br>";
+    echo "Prodi : ". $mhs2->getProdi() ."<br>";
+    echo "Semester : ". $mhs2->getSemester() ."<br>";
+
+    
+
+?>
