943bf09fa3e0799016cd6bad2c27396a7ee15815
giovanialvin <99602640+giovanialvin@users.noreply.github.com>
2022-02-14 21:52:18 +0700

    Add files via upload

---
+++ Poin B/C++/SoccerClub.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class SoccerClub{
+    private:
+        //atribut
+        string name;
+        string nation;
+        string year;
+        vector<string> players;
+
+    public:
+        SoccerClub(){
+        }
+        SoccerClub(string namec, string nationc, string yearc, vector<string> playersc){
+            this->name = namec;
+            this->nation = nationc;
+            this->year = yearc;
+            this->players.insert(this->players.end(), playersc.begin(), playersc.end());
+        };
+
+        //get set methods
+        void setName(string namec){
+            this->name = namec;
+        }
+        string getName(){
+            return this->name;
+        }
+        void setNation(string nationc){
+            this->nation = nationc;
+        }
+        string getNation(){
+            return this->nation;
+        }
+        void setYear(string yearc){
+            this->year = yearc;
+        }
+        string getYear(){
+            return this->year;
+        }
+        void setPlayers(vector<string> playersc)
+        {
+            this->players.insert(this->players.end(), playersc.begin(), playersc.end());
+        }
+        vector<string> getPlayers(){
+            return this->players;
+        }
+        
+
+        void output(){
+            cout << "Name Team      : " + this->getName() << endl;
+            cout << "Country        : " + this->getNation() << endl;
+            cout << "Established    : " + this->getYear() << endl;
+            cout << "Players        : " << endl;
+            int i = 0;
+
+            for (i = 0; i<this->players.size(); i++){
+                cout << "                   - " + this->players[i] << endl;
+            }
+    	}
+};
---
+++ Poin B/C++/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "SoccerClub.cpp"
+
+using namespace std;
+
+int main(){
+    //input cara 1
+	SoccerClub Team1;
+    //masukkan nilai vektor dari belakang
+	vector<string> arrPlayers;
+    arrPlayers.push_back("Okoye");
+    arrPlayers.push_back("N'Jobu");
+    arrPlayers.push_back("Nakia");
+
+	Team1.setName("Wakanda FC");
+	Team1.setNation("Wakanda");
+	Team1.setYear("1991");
+	Team1.setPlayers(arrPlayers);
+
+	// output
+	Team1.output();
+
+    cout << endl;
+
+    //input cara 2
+    //masukkan nilai vektor dari belakang
+	vector<string> arrPlayers2;
+	arrPlayers2.push_back("Arthur");
+	arrPlayers2.push_back("Clark Kent");
+	arrPlayers2.push_back("Bruce Wayne");
+	arrPlayers2.push_back("Zoro");
+
+    SoccerClub Team2("Justice Football Team", "DCEU", "1988", arrPlayers2);
+    Team2.output();
+
+    return 0;
+}
